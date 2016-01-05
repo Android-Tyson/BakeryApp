bdcc0ccda2ac4094d90853f4c4ee65a9dcb85c78
dev-bpn <jazzbpn@gmail.com>
2016-01-05 12:46:18 +0545

    Toolbar on Fragment Done

--- app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/NormalRegister.java
+++ app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/NormalRegister.java
@@ -1,18 +1,27 @@
 package com.urbangirlbakeryandroidapp.alignstech.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.urbangirlbakeryandroidapp.alignstech.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * A simple {@link Fragment} subclass.
  */
+
 public class NormalRegister extends android.support.v4.app.Fragment {
+
+    @InjectView(R.id.app_toolbar)
+    Toolbar toolbar;
 
     public static NormalRegister newInstance(){
         NormalRegister normalRegister = new NormalRegister();
@@ -28,7 +37,11 @@ public class NormalRegister extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_normal_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_normal_register, container, false);;
+        ButterKnife.inject(this , view);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.user_info);
+        return view;
     }
 
 
