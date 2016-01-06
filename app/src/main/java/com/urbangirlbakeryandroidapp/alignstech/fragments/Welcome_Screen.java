ffe70d4a7194fb716388a6ba8bed0c316b005509
dev-bpn <jazzbpn@gmail.com>
2016-01-06 14:24:56 +0545

    UserPic for SucceedFragment

--- app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/Welcome_Screen.java
+++ app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/Welcome_Screen.java
@@ -1,20 +1,34 @@
 package com.urbangirlbakeryandroidapp.alignstech.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.TextView;
 
+import com.android.volley.toolbox.NetworkImageView;
 import com.urbangirlbakeryandroidapp.alignstech.R;
+import com.urbangirlbakeryandroidapp.alignstech.utils.MySingleton;
+
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Welcome_Screen extends DialogFragment {
+
+    @InjectView(R.id.user_circular_imageView)
+    NetworkImageView user_pic;
+
+    @InjectView(R.id.user_phone)
+    TextView user_phone;
+
+    @InjectView(R.id.user_mail)
+    TextView user_mail;
 
     public static Welcome_Screen newInstance(){
 
@@ -33,7 +47,9 @@ public class Welcome_Screen extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        return inflater.inflate(R.layout.fragment_welcome__screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_welcome__screen, container, false);
+        user_pic.setImageUrl("PROFILE_URL", MySingleton.getInstance(getActivity()).getImageLoader());
+        return view;
     }
 
 
