b8b6f36a25f72c07ed6bdd6b0143eec60d188309
dev-bpn <jazzbpn@gmail.com>
2016-01-13 13:26:27 +0545

    Inilializing stuffs

--- app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/HomeFragment.java
+++ app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/HomeFragment.java
@@ -6,13 +6,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.urbangirlbakeryandroidapp.alignstech.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends android.support.v4.app.Fragment {
+
+    @InjectView(R.id.textView_categories_1)
+    TextView categories_1;
+
+    @InjectView(R.id.textView_categories_2)
+    TextView categories_2;
+
+    @InjectView(R.id.textView_categories_3)
+    TextView categories_3;
+
+    @InjectView(R.id.see_more_categories)
+    RelativeLayout see_more_categories;
+
+    @InjectView(R.id.textView_gift_1)
+    TextView gift_1;
+
+    @InjectView(R.id.textView_gift_2)
+    TextView gift_2;
+
+    @InjectView(R.id.textView_gift_3)
+    TextView gift_3;
+
+    @InjectView(R.id.see_more_gift)
+    RelativeLayout see_more_gift;
 
     public static HomeFragment newInstance(int position){
 
@@ -33,6 +62,8 @@ public class HomeFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.inject(this , view);
+        return view;
     }
 }
