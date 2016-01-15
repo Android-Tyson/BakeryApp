c47d31892291eaebe245224c95b00e49cd9734ac
dev-bpn <jazzbpn@gmail.com>
2016-01-15 16:49:40 +0545

    Apis MAKEUP

--- app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/utils/Apis.java
+++ app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/utils/Apis.java
@@ -9,10 +9,10 @@ public class Apis {
     public static final String userDetialPostURl = BASE_URL + "userRegister";
     public static final String nav_collection = BASE_URL + "collection";
     public static final String some_categories_list = "http://cake.yogeshojha.com.np/api/child-categories/5?n=3";
-    public static final String some_gift_list = "http://cake.yogeshojha.com.np/api/child-categories/3?n=3";
-    public static final String headerImageSlider = "http://cake.yogeshojha.com.np/api/categories";
-    public static final String urgent_cake = "http://cake.yogeshojha.com.np/api/urgent-cake";
-    public static final String see_all_categories = "http://cake.yogeshojha.com.np/api/child-categories/5";
-    public static final String see_all_gifts = "http://cake.yogeshojha.com.np/api/child-categories/3";
+    public static final String some_gift_list = BASE_URL + "api/child-categories/3?n=3";
+    public static final String headerImageSlider = BASE_URL + "api/categories";
+    public static final String urgent_cake = BASE_URL + "api/urgent-cake";
+    public static final String see_all_categories = BASE_URL + "api/child-categories/5";
+    public static final String see_all_gifts = BASE_URL + "api/child-categories/3";
 
 }
