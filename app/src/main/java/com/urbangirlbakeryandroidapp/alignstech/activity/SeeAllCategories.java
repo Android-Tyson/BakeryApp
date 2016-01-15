efa664b08284f070bb13779ce8836dc6ffc02841
dev-bpn <jazzbpn@gmail.com>
2016-01-15 14:53:31 +0545

    Data in ListView after Response

--- app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/activity/SeeAllCategories.java
+++ app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/activity/SeeAllCategories.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.squareup.otto.Subscribe;
 import com.urbangirlbakeryandroidapp.alignstech.R;
@@ -15,7 +17,11 @@ import com.urbangirlbakeryandroidapp.alignstech.utils.AppController;
 import com.urbangirlbakeryandroidapp.alignstech.utils.MyBus;
 import com.urbangirlbakeryandroidapp.alignstech.utils.MyUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -24,6 +30,9 @@ public class SeeAllCategories extends AppCompatActivity {
 
     @InjectView(R.id.app_toolbar)
     Toolbar toolbar;
+
+    @InjectView(R.id.listView)
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +63,25 @@ public class SeeAllCategories extends AppCompatActivity {
 
         JSONObject jsonObject = event.getJsonObject();
         MyUtils.showLog(jsonObject.toString());
+        performJsonTaskForCategories(jsonObject);
 
+    }
+
+    private void performJsonTaskForCategories(JSONObject jsonObject) {
+
+        ArrayList<String> giftChildList = new ArrayList<>();
+
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("result");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObj = jsonArray.getJSONObject(i);
+                String singleChildname = jsonObj.getString("name");
+                giftChildList.add(singleChildname);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, giftChildList));
     }
 
     @Override
