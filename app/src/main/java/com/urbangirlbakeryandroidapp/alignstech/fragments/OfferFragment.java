d3320bd91a78100752d3697448d572e478ab0d50
dev-bpn <jazzbpn@gmail.com>
2016-01-18 14:07:11 +0545

    GetOffer DONE

--- app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/OfferFragment.java
+++ app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/fragments/OfferFragment.java
@@ -1,41 +1,108 @@
 package com.urbangirlbakeryandroidapp.alignstech.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.squareup.otto.Subscribe;
 import com.urbangirlbakeryandroidapp.alignstech.R;
-import com.urbangirlbakeryandroidapp.alignstech.controller.GetChildCollectionList;
-import com.urbangirlbakeryandroidapp.alignstech.utils.DataBase_Utils;
+import com.urbangirlbakeryandroidapp.alignstech.adapter.CustomListItemAdapter;
+import com.urbangirlbakeryandroidapp.alignstech.bus.SeeAllGiftsEvent;
+import com.urbangirlbakeryandroidapp.alignstech.controller.GetSeeAllGifts;
+import com.urbangirlbakeryandroidapp.alignstech.utils.Apis;
+import com.urbangirlbakeryandroidapp.alignstech.utils.AppController;
+import com.urbangirlbakeryandroidapp.alignstech.utils.MyBus;
+import com.urbangirlbakeryandroidapp.alignstech.utils.MyUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OfferFragment extends android.support.v4.app.Fragment {
 
+    @InjectView(R.id.listView)
+    ListView listView;
+
+    private ArrayList<String> childIdList = new ArrayList<>();
 
     public OfferFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MyBus.getInstance().register(this);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_offer, container, false);
+        View view =  inflater.inflate(R.layout.fragment_offer, container, false);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if(!DataBase_Utils.isOfferListDataExists()) {
-            GetChildCollectionList.parseNavigationDrawerList(getActivity());
-        }else {
-
+        if(MyUtils.isNetworkConnected(getActivity())){
+            parseAllGifts();
         }
     }
+
+    private void parseAllGifts(){
+        if(MyUtils.isNetworkConnected(getActivity())){
+            GetSeeAllGifts.parseAllGiftList(Apis.see_all_gifts, getActivity());
+        }
+    }
+
+    @Subscribe
+    public void seeAllGifts(SeeAllGiftsEvent event){
+
+        JSONObject jsonObject = event.getJsonObject();
+        performJsonTaskForGifts(jsonObject);
+
+    }
+
+    private void performJsonTaskForGifts(JSONObject jsonObject) {
+
+        ArrayList<String> giftChildList = new ArrayList<>();
+
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("result");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObj = jsonArray.getJSONObject(i);
+                String singleChildname = jsonObj.getString("name");
+                String singleChildId = jsonObj.getString("id");
+                giftChildList.add(singleChildname);
+                childIdList.add(singleChildId);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        listView.setAdapter(new CustomListItemAdapter(getActivity(), giftChildList));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        MyBus.getInstance().unregister(this);
+        AppController.getInstance().cancelPendingRequests("GET_ALL_GIFT_TAG");
+    }
+
 }
