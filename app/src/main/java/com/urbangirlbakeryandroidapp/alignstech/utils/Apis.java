78b824f8a8f00473191c47db1cc0e7d814ba26ba
dev-bpn <jazzbpn@gmail.com>
2016-01-18 13:32:54 +0545

    makeup

--- app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/utils/Apis.java
+++ app/src/main/java/com/urbangirlbakeryandroidapp/alignstech/utils/Apis.java
@@ -8,7 +8,7 @@ public class Apis {
     public static final String BASE_URL = "http://cake.yogeshojha.com.np/";
     public static final String userDetialPostURl = BASE_URL + "userRegister";
     public static final String nav_collection = BASE_URL + "collection";
-    public static final String some_categories_list = "http://cake.yogeshojha.com.np/api/child-categories/5?n=3";
+    public static final String some_categories_list = BASE_URL + "api/child-categories/5?n=3";
     public static final String some_gift_list = BASE_URL + "api/child-categories/3?n=3";
     public static final String headerImageSlider = BASE_URL + "api/categories";
     public static final String urgent_cake = BASE_URL + "api/urgent-cake";
